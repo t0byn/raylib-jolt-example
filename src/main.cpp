cc6ab12078bc730f517401450ea590e1c5638027
t0byn <tobyn112@outlook.com>
2024-12-19 23:24:19 +0800

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <Jolt/Jolt.h>
+
+// Jolt includes
+#include <Jolt/RegisterTypes.h>
+#include <Jolt/Core/Factory.h>
+#include <Jolt/Core/TempAllocator.h>
+#include <Jolt/Core/JobSystemThreadPool.h>
+#include <Jolt/Physics/PhysicsSettings.h>
+#include <Jolt/Physics/PhysicsSystem.h>
+#include <Jolt/Physics/Collision/Shape/BoxShape.h>
+#include <Jolt/Physics/Collision/Shape/SphereShape.h>
+#include <Jolt/Physics/Body/BodyCreationSettings.h>
+#include <Jolt/Physics/Body/BodyActivationListener.h>
+
+JPH_SUPPRESS_WARNINGS
+
+// raylib includes
+#include <raylib.h>
+
+#define UNUSED_VAR(x) ((void)(x))
+
+int main(int argc, char** argv)
+{
+    UNUSED_VAR(argc);
+    UNUSED_VAR(argv);
+
+    return 0;
+}
